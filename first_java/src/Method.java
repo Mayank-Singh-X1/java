4068c1216ddf6a3f5a5d0000c37aa23738acbbe7
Mayank-Singh <mayankbrawler@gmail.com>
2025-01-26 14:14:42 +0530

    new

---
+++ first_java/src/Method.java
@@ -0,0 +1,15 @@
+public class Method {
+  static int fuc( int x,int y){
+    int c;
+    return c= x+y;
+    
+
+  }
+  public static void main(String[] args){
+    
+    int a=4;
+    int b=4;
+    System.out.println(fuc(a,b));
+
+  }
+}
