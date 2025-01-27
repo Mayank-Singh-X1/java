4cd7c353b967dcf30d790613efb32329f323e089
Mayank-Singh <mayankbrawler@gmail.com>
2025-01-27 14:34:23 +0530

    method

--- first_java/src/Method.java
+++ first_java/src/Method.java
@@ -4,23 +4,37 @@ public class Method {
   // arr[2]=34;  
   // }
 
-  static void overloading (){
-    System.out.println("good morning broh!!");
+  // static void overloading (){
+  //   System.out.println("good morning broh!!");
+  // }
+
+  // static void overloading(int a){
+  //   System.out.println("good morning " +a +" broh!!");
+  // }
+
+  static int sum(int ...arr){
+    int result=0;
+    for(int a:arr){
+      result+=a;
+    }
+    return result;
+
   }
-  static void overloading(int a){
-    System.out.println("good morning " +a +" broh!!");
-  }
+
+
+
 
   public static void main(String[] args){
 
-  // int dgye[]={2,4,6,34,04};
-  //  telljoke(dgye);
-  //  System.out.println(dgye[2]);
+      // int dgye[]={2,4,6,34,04};
+      //  telljoke(dgye);
+      //  System.out.println(dgye[2]);
 
   //methods overloading
-  overloading();
-  overloading(1000);
+      // overloading();
+      // overloading(1000);
 
+      System.out.println("The sum of 4,76, 5 is: "+sum(4,76,5));
 
 
   }
