6be9e53aef5b505b58685ff38c0ee92aa1706925
Mayank-Singh <mayankbrawler@gmail.com>
2025-09-12 10:52:24 +0530

    dsa lec 4

---
+++ DSA.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class DSA {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the number of values: ");
+        int n=sc.nextInt();
+        int i=0;
+        while(i<10){
+            i++;
+            int sum=i*n;
+        System.out.println(sum+"");  
+        }
+        
+
+
+
+
+
+
+        sc.close();
+    }
+}
