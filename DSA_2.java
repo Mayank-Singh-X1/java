178c411f5acc9f1627956d5330981219b27db23c
Mayank-Singh <mayankbrawler@gmail.com>
2025-09-17 23:04:04 +0530

    gonna start complexity

--- DSA_2.java
+++ DSA_2.java
@@ -1,23 +1,35 @@
 import java.util.*;
 public class DSA_2 {
 
-public static int printMyName(int a){
-    int fact=1;
-    for(int i=a;i>=1;i--){
-        fact=fact*i;
+    public static void  returnCount(int x){
+       int p=0,n=0,z=0;
+        if(x<0){     
+           n++; 
+        }else if(x>0){          
+            p++;
+        }else{         
+            z++;
+        }
+        System.out.println("pos:"+p+" neg:"+n+" zero:"+z);
     }
-    return fact;
+
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        
+        boolean Action= true;
+        int x;
+        
+        while(Action){
+            
+            System.out.println("Enter number or type \"quit\" to exit: ");
+            x=sc.nextInt();
+            if(x=="quit"){
+                break;
+            }
+           returnCount(x); 
+        }
+
+
+    }
 }
 
-public static void main(String [] args){
-Scanner sc=new Scanner (System.in);
-
-int x=sc.nextInt();
-int y=sc.nextInt();
-
-System.out.println( printMyName(x));
-
-sc.close();
-
-}
-}
