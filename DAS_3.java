f7d8e3986d0383e9f624189b907bb679e2e79423
Mayank-Singh <mayankbrawler@gmail.com>
2025-09-18 11:25:12 +0530

    lecture 10

---
+++ DAS_3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class DAS_3 {
+    public static void main(String[] args) {
+        //arrays
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter index "+(i+1)+" number: ");
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("enter the number you want to search: ");
+        int key=sc.nextInt();
+      
+       
+        for(int i=0;i<arr.length;i++){
+        if(key==arr[i]){
+            System.out.println("found at index : "+arr[i]);
+        };
+        }
+        sc.close();
+    }
+}
