17742425a7eef7eb607c0a69be0fa15338673bdd
Mayank-Singh <mayankbrawler@gmail.com>
2025-10-17 18:20:19 +0530

    recursion 1 completed

--- Recursion.java
+++ Recursion.java
@@ -1,25 +1,48 @@
 public class Recursion {
-public static void printnum(int n){
-   if(n==0){
-    return;
-   }
-   System.out.println(n);
-    printnum(n-1);
-}
+    // public static void printnum(int n) {
+    // if (n == 6) {
+    // return;
+    // }
+    // System.out.println(n);
+    // printnum(n + 1);
+    // }
+
+    // sum of n natural numbes using recursion
+    // public static void printsum(int i,int n,int sum) {
+    // sum+=i;
+    // if(i==n){
+    // System.out.println(sum);
+    // return;
+    // }
+    // printsum(i+1, n, sum);
+    // }
+
+    // factorial
+    // public static void factorial(int i, int n, int fact) {
+    //     fact *= i;
+    //     if (i == n) {
+    //         System.out.println(fact);
+    //         return;
+    //     }
+    //     factorial(i + 1, n, fact);
+
+    // }
+
+    //fibonacci
+    // public static void fibonacci(int a, int b,int n ) {
+        
+        
+    //     if(n==0){
+    //         return;
+    //     }
+    //     int c=a+b;
+    //     System.out.println(c);
+    //     fibonacci(b, c, n-1);
+    // }
+
 
     public static void main(String[] args) {
-  int n=6;
-  printnum(n);
-
-
-  
-  System.out.println("Done");
-}}
-
-
-
-
-
-
-
-
+       
+        System.out.println("Done");
+    }
+}
