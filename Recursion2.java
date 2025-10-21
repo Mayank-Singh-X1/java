375980d541a49e8bf71b1aaa537f3ffd942f778a
Mayank-Singh <mayankbrawler@gmail.com>
2025-10-21 21:16:51 +0530

    practised recursions techniques

--- Recursion2.java
+++ Recursion2.java
@@ -1,20 +1,56 @@
 public class Recursion2 {
 
-    public static void towerOfHanoi(int n, String src, String helper, String dest) {
-        if (n == 1) {
-            System.out.println("move disk " + n + " from " + src + " to " + dest);
+    // public static void towerOfHanoi(int n, String src, String helper, String dest) {
+    //     if (n == 1) {
+    //         System.out.println("move disk " + n + " from " + src + " to " + dest);
+    //         return;
+    //     }
+    //     towerOfHanoi(n - 1, src, dest, helper);
+    //     System.out.println("move disk " + n + " from " + src + " to " + dest);
+    //     towerOfHanoi(n - 1, helper, src, dest);
+    // }
+
+    // public static void reverse(String str,int index){
+    //     if(index ==0){
+    //         System.out.print(str.charAt(index));
+    //         return;
+    //     }
+    //     System.out.print(str.charAt(index));
+    //     reverse(str, index-1);
+    // }
+    public static int first=-1;
+    public static int last=-1;
+    public static void findchar(String str,char key,int index){
+        if(index==str.length()){
+            System.out.println("First occurrence: " + first);
+            System.out.println("Last occurrence: " + last);
             return;
         }
-        towerOfHanoi(n - 1, src, dest, helper);
-        System.out.println("move disk " + n + " from " + src + " to " + dest);
-        towerOfHanoi(n - 1, helper, src, dest);
-
+        char currentChar=str.charAt(index);
+        if(currentChar==key){
+            if(first==-1){
+                first=index;
+            }else{
+                last=index;
+            }
+        }
+        findchar(str, key, index + 1);  // Changed index++ to index + 1
     }
 
     public static void main(String[] args) {
-        int n = 3;
-        towerOfHanoi(n, "S", "H", "D");
 
+        //Tower Of Hanoi
+        // int n = 3;
+        // towerOfHanoi(n, "S", "H", "D");
+
+        //Print word in reverse
+        // String a="leh reh lund keh";
+        // reverse(a,a.length()-1);
+
+        //Find first and last index of character
+        String a="Sanat Clausahk";
+        findchar(a, 'a', 0);
+   
     }
 
 }
--- quickSort.java
+++ quickSort.java
@@ -1,11 +1,12 @@
 public class quickSort {
 
-     public static int partition(int arr[], int low,int high) {
+    public static int partition(int arr[], int low, int high) {
         int pivot = arr[high];
         int i = low;
         for (int j = low; j < high; j++) {
             if (arr[j] < pivot) {
                 int temp = arr[i];
+
                 arr[i] = arr[j];
                 arr[j] = temp;
                 i++;
@@ -17,24 +18,24 @@ public class quickSort {
         return i;
     }
 
-    public static void quickS(int arr[],int low, int high){
-        if(low<high){
-          int pivot=partition(arr, low, high);
-          quickS(arr, low, pivot-1);
-          quickS(arr, pivot+1, high);   
+    public static void quickS(int arr[], int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+
+            quickS(arr, low, pivot - 1);
+            quickS(arr, pivot + 1, high);
         }
     }
-   
 
     public static void main(String[] args) {
-        int arr[]={8,5,3,2,7,1,5,9};
-        int n=arr.length;
-        quickS(arr, 0, n-1);
+        int arr[] = { 8, 5, 3, 2, 7, 1, 5, 9 };
+        int n = arr.length;
+        quickS(arr, 0, n - 1);
 
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+", ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + ", ");
         }
         System.out.println();
     }
-    
+
 }
